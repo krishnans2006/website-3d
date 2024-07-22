2d23737ad535086a2692bceb2a48d60d171572ff
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-22 01:42:25 -0400

    Add a line and improve naming

--- main.js
+++ main.js
@@ -8,10 +8,19 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+const cube_geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const cube_material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( cube_geometry, cube_material );
 scene.add( cube );
+
+const line_material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const line_points = [];
+line_points.push( new THREE.Vector3( - 10, 0, 0 ) );
+line_points.push( new THREE.Vector3( 0, 10, 0 ) );
+line_points.push( new THREE.Vector3( 10, 0, 0 ) );
+const line_geometry = new THREE.BufferGeometry().setFromPoints( line_points );
+const line = new THREE.Line( line_geometry, line_material );
+scene.add( line );
 
 camera.position.z = 5;
 
